0b3a4156c4ebfdca86b3170d74f2a4b14e756a86
eusanchez <anaeu_16@hotmail.com>
2023-02-03 19:04:14 -0600

    agrego tension DC solo para el CANAL A, modifico etiquetas del simu

--- Laboratorio3/clase/clase.ino
+++ Laboratorio3/clase/clase.ino
@@ -5,22 +5,22 @@
 Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 
 //Definicion de variables globales
-float V1= {0.00};
-float v1k = {0.00};
+float VA= {0.00};
+float vAk = {0.00};
 float boton = {0.00};
 float interruptor = {0.00};
 float k1= {4.8};
 float threshold = {1.00};
 bool etiqueta = true;
 
-float current1 = {0.00};
+float corrienteA = {0.00};
 
 
 //Funcion de Setup para los puertos y pantalla 
 void setup(){
   Serial.begin(9600); //Inicio del puerto Serial
   //Configira los pines como salida
-  pinMode(10,OUTPUT);
+  pinMode(9,OUTPUT);
   //Inicia la pantalla y se selecciona el contraste
   display.begin();
   display.setContrast(75);
@@ -46,8 +46,8 @@ void setup(){
 //Funcion encargada de encender los leds de emergencia 
   
 //Como la funcion AC devuelve valores RMS es importante hacer una funcion de alarma con los valores RMS
-void precaucion_AC(float v1){
-  if((v1 >= 16.9)){ //23.9/sqrt(2) = 16.9
+void precaucion_AC(float vA){
+  if((vA >= 16.9)){ //23.9/sqrt(2) = 16.9
       digitalWrite(9, HIGH);
     }
   else{
@@ -56,10 +56,10 @@ void precaucion_AC(float v1){
 
 //------------------------------------------- Funciones para el calculo del valor maximo en modo AC ---------------------------------------\\
 //Obtiene el valor de la amplitud de la onda para el primer puerto
-float get_max_v1() {
+float get_max_vA() {
   float max_v = 0.00;
   for(int i = 0; i < 100; i++) {
-    float r = analogRead(A1);  
+    float r = analogRead(A3);  
     if(max_v < r) max_v = r;
     delayMicroseconds(200);
   }
@@ -71,6 +71,7 @@ void loop(){
   boton = analogRead(A4); // Revisa si el boton de modo (AC/DC) esta encendido
   interruptor = analogRead(A5); //Transmision
 
+
   if (interruptor > 3.00){
     while (etiqueta){
       Serial.println("Canal 1");
@@ -81,16 +82,16 @@ void loop(){
   
   if (boton > 3.00){ // Si el boton esta encendido mida AC
     
-    float VAC1 = get_max_v1();
+    float VAC1 = get_max_vA();
     VAC1 = ((((VAC1*5)/1023)*k1)-12);
     if ((VAC1+0.65) == 12) VAC1 += 0.65;  
     VAC1 /= sqrt(2); // Obtiene el valor RMS
 
     if (interruptor > 3.00){
-      if ((current1 >= VAC1+threshold || current1 <= VAC1-threshold)){
+      if ((corrienteA >= VAC1+threshold || corrienteA <= VAC1-threshold)){
         Serial.println(VAC1);
         Serial.println("AC");
-        current1 = VAC1;
+        corrienteA = VAC1;
       }
     }
 
@@ -98,14 +99,38 @@ void loop(){
     
     display.print("Voltimetro AC \n");
     display.print("--------------");
-    display.print("V1 :");
-    display.print(VAC1);
-    display.print(" Vrms");
-    display.print("\n");
+    display.print("vA: ", VAC1, " Vrms\n");
     display.display();
     display.clearDisplay();
 
     precaucion_AC(VAC1);
     
+  } else{ // si switch esta cerrado mide DC
+
+    //Valores DC 
+    vA = analogRead(A3);
+
+
+     //Transformar el rango de 0 a 5 (rango aceptado por Arduino) a 0 a 24 (rango requerido)
+    vAk = (((vA*5)/1023)*k1)-12;
+
+    if (interruptor > 3.00){
+      if (corrienteA >= vAk+threshold || corrienteA <= vAk-threshold){
+        Serial.println(vAk);
+        Serial.println("DC");
+        corrienteA = vAk;
+      }
+    }
+    //Pantalla 
+  
+    display.print("Voltimetro DC \n");
+    display.print("--------------");
+    display.print("vA: ");
+    display.print(vAk,"V\n");
+    display.display();
+    display.clearDisplay();
+
+    precaucion_DC(vAk);
   }
 }
+
