8c50a3f7fcbace2787ee326fc60901805939067e
adrian <adrianmonte2010@hotmail.com>
2023-02-15 22:59:54 -0600

    Definicion de limites de los servos. Prueba con LDRs exitosa.

--- solar_tracker/solar_tracker.ino
+++ solar_tracker/solar_tracker.ino
@@ -1,25 +1,59 @@
 // Include the Servo library 
 #include <Servo.h> 
-// Declare the Servo pin 
-int servoPin2 = 2; int servoPin3 = 3; 
-// Create a servo object 
-Servo Servo2, Servo3; 
+
+// Creamos dos objetos tipo 'servo', uno del eje horizontal y otro para el vertical. 
+Servo servo_h, servo_v; 
+
+// Declaramos variables globales para definir los limites de operacion de los servos.
+
+// Limites del servo horizontal.
+int position_h = 180; 
+int servo_h_LimitLow = 10;
+int servo_h_LimitHigh = 170;
+
+
+// Limites del servo vertical.
+int position_v = 60; 
+int servo_v_LimitLow = 1;
+int servo_v_LimitHigh = 90;
+
+
+
 void setup() { 
-   // We need to attach the servo to the used pin number 
-   Servo2.attach(servoPin2); 
-   Servo3.attach(servoPin3); 
+  // Pegamos los 2 servos a algún pin de I/O para poder controlarlos; se conectan al pin 2 y 3, respectivamente.
+  servo_h.attach(2);
+  servo_v.attach(3);
+  Serial.begin(9600);
+
+  // Configuramos por defecto la angulación de los servos en 180 y 45 grados. 
+  servo_h.write(0);
+  servo_v.write(0);
+  delay(2500);
 }
+
 void loop(){ 
-   // Make servo go to 0 degrees 
-   Servo2.write(0); 
-   Servo3.write(0); 
-   delay(1000); 
-   // Make servo go to 90 degrees 
-   Servo2.write(90); 
-   Servo3.write(90); 
-   delay(1000); 
-   // Make servo go to 180 degrees 
-  Servo2.write(180); 
-  Servo3.write(180); 
-   delay(1000); 
+  int TL = analogRead(A1); // top left
+  int TR = analogRead(A2); // top right
+  int BL = analogRead(A3); // down left
+  int BR = analogRead(A4); // down right
+  
+  Serial.print("Valor analogico de LDR TL: ");
+  Serial.print(TL);
+  Serial.print("\n");
+  if (TL > 500 ) servo_v.write(180);
+  else servo_v.write(90);
+
+  Serial.print("Valor analogico de LDR BR: ");
+  Serial.print(BR);
+  Serial.print("\n");
+  if (BR > 500) servo_h.write(180);
+  else servo_h.write(90);
+
+  delay(1000);
 }
+
+
+
+
+
+
