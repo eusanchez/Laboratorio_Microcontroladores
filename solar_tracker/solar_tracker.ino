268d9d53e6022fb0bdf4b837f8335fb981c39321
Adrian Montero Bonilla <adrianmonte2010@hotmail.com>
2023-02-27 09:13:17 -0600

    Firmware actualizado, funcional.

--- solar_tracker/solar_tracker.ino
+++ solar_tracker/solar_tracker.ino
@@ -13,9 +13,12 @@ int servo_h_LimitHigh = 180;
 
 
 // Limites del servo vertical.
-int position_v = 60; 
+int position_v = 0; 
 int servo_v_LimitLow = 0;
 int servo_v_LimitHigh = 180;
+
+int speed = 2; // Velocidad de operacion de los servos, conviene dejarlo entre 5-10
+int tolerance = 25; // Intervalo decentre entre 50-100
 
 
 
@@ -29,30 +32,36 @@ void setup() {
   servo_h.write(0);
   servo_v.write(0);
   delay(2000);
+  
 }
 
-void horizontal_rotation(){
-
-}
 
 void loop(){ 
+  
   int TL = analogRead(A1); // top left
   int TR = analogRead(A2); // top right
   int BL = analogRead(A3); // bottom left
   int BR = analogRead(A4); // bottom right
 
-  /* Calculamos los promedios de los lados (arriba, abajo, izquierda y derecha)*/
+  // Calculamos los promedios de los lados (arriba, abajo, izquierda y derecha)
   int average_top = (TL + TR) / 2;
   int average_bottom = (BL + BR) / 2;
   int average_left = (TL + BL) / 2;
   int average_right = (TR + BR) / 2;
 
+  // Diferencias entre las 4 direcciones para comparar con la tolerancia escogida
+  int diff_horizontal = abs(average_left - average_right);
+  int diff_vertical = abs(average_top - average_bottom);
+
+
   // Comenzamos a comparar los promedios de luz que se reciben de los LDRs
 
 
   // Primeros controlamos el servo horizontal (servo ubicado en la base)
-  if (average_left > average_right) position_h -= 20 ;
-  else position_h += 20;
+  if (diff_horizontal > tolerance) {
+    if (average_left > average_right) position_h += speed ;
+    else position_h -= speed;
+  }
 
   if (position_h < servo_h_LimitLow) position_h = servo_h_LimitLow;
   if (position_h > servo_h_LimitHigh) position_h = servo_h_LimitHigh;
@@ -60,48 +69,26 @@ void loop(){
 
   // Seguimos con la logica del servo vertical (servo que gira el panel solar de arriba hacia abajo)
   
-  if (average_top > average_bottom) position_v -= 20 ;
-  else position_v += 20;
+  if (diff_vertical > tolerance) {
+    if (average_top > average_bottom) position_v -= speed ;
+    else position_v += speed;
+  }
 
   if (position_v < servo_v_LimitLow) position_v = servo_v_LimitLow;
   if (position_v > servo_v_LimitHigh) position_v = servo_v_LimitHigh;
   servo_v.write(position_v);
 
-
-
-
-
-  
-  Serial.print("Valor analogico de LDR TL: ");
-  Serial.print(TL);
-  Serial.print("\n");
-  
-
-  Serial.print("Valor analogico de LDR TR: ");
-  Serial.print(TR);
-  Serial.print("\n");
-
-  Serial.print("Valor analogico de LDR BL: ");
-  Serial.print(BL);
-  Serial.print("\n");
-
-  Serial.print("Valor analogico de LDR BR: ");
-  Serial.print(BR);
-  Serial.print("\n");
-
- 
-
-  Serial.print("Left average: ");
-  Serial.print(average_left);
-  Serial.print("\n");
-
-  Serial.print("Right average: ");
-  Serial.print(average_right);
-  Serial.print("\n");
   
   
+  // Leemos el puerto A5, procesamos senal analogica y la pasamos a trave sdel puerto serial
+  float solar_out = (analogRead(A5) *5 ) / 1023; // Pasamos lectura de 0-1023 a 0-5V
 
-  //delay(1000);
+  Serial.print("Vout: ");
+  Serial.print(solar_out);
+  Serial.print("\n");
+  
+  delay(25);
+  
 }
 
 
